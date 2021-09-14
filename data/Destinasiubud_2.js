75f2e2fd354bd4156f5d9f24b5c3a2a019b4e8da
Rissa Rambe <71384953+rissarambe@users.noreply.github.com>
2021-09-14 16:21:58 +0700

    Add files via upload

---
+++ data/Destinasiubud_2.js
@@ -0,0 +1,12 @@
+var json_Destinasiubud_2 = {
+"type": "FeatureCollection",
+"name": "Destinasiubud_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Campuhan Ridge Walk, Jalan Raya Campuhan, Sayan, Gianyar, Bali", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2553657, -8.5041441 ] } },
+{ "type": "Feature", "properties": { "Name": "Ubud Yoga House", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2582024, -8.502974 ] } },
+{ "type": "Feature", "properties": { "Name": "Earth Cafe & Market Ubud", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2642382, -8.5118167 ] } },
+{ "type": "Feature", "properties": { "Name": "Ubud Bodyworks Centre", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2646032, -8.5112387 ] } },
+{ "type": "Feature", "properties": { "Name": "Folk Pool & Gardens", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2606391, -8.5139348 ] } }
+]
+}
---
+++ data/fasilitasubud_3.js
@@ -0,0 +1,12 @@
+var json_fasilitasubud_3 = {
+"type": "FeatureCollection",
+"name": "fasilitasubud_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Ubud Clinic", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2555322, -8.5046921 ] } },
+{ "type": "Feature", "properties": { "Name": "Puskesmas Ubud I", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.262681, -8.510112 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Resor Gianyar Polda Bali", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2715923, -8.5077237 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Ubud", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2718176, -8.505039146 ] } },
+{ "type": "Feature", "properties": { "Name": "Pura Desa Ubud", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 115.2619447, -8.5065114 ] } }
+]
+}
